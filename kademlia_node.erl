588fa53bbd1fc38934a43bd56ff3c8b23e35b230
Zack9961 <mrzack9099@gmail.com>
2025-01-22 18:10:32 +0100

    gen_server

--- kademlia_node.erl
+++ kademlia_node.erl
@@ -1,27 +1,38 @@
 -module(kademlia_node).
--export([start_kademlia_node/0, ping/1, loop/0]).
+-behaviour(gen_server).
+-export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, stop/0]).
+-export([ping/1]).
 
-start_kademlia_node() ->
-    spawn(kademlia_node, loop, []).
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, ok, []).
 
-loop() ->
-    receive
-        {ping, Sender} ->
-            io:format("Node ~p received ping from ~p~n", [self(), Sender]),
-            Sender ! {pong, self()},
-            loop();
-        Other ->
-            io:format("Node ~p received unknown message: ~p~n", [self(), Other]),
-            loop()
-    end.
+init(State) ->
+    io:format("Kademlia node starting, initial state: ~p~n", [State]),
+    {ok, State}.
+
+handle_call({ping, Sender}, _From, State) ->
+    io:format("Node ~p received ping from ~p~n", [self(), Sender]),
+    Sender ! {pong, self()},
+    {reply, ok, State};
+handle_call(stop, _From, State) ->
+    {stop, normal, State};
+handle_call(Other, _From, State) ->
+    io:format("Node ~p received unknown call: ~p~n", [self(), Other]),
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    io:format("Node ~p received info: ~p~n", [self(), _Info]),
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    io:format("Node ~p terminating~n", [self()]),
+    ok.
 
 ping(Node) ->
-    Node ! {ping, self()},
-    receive
-        {pong, Node} ->
-            io:format("Node ~p received pong from ~p~n", [self(), Node]),
-            ok
-    after 1000 ->
-        io:format("Node ~p no pong received~n", [self()]),
-        timeout
-    end.
+    gen_server:call(Node, {ping, self()}, 1000).
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
