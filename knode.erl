071a6de45ac3912e3bda54f0884be3f6162f5a56
Zack9961 <mrzack9099@gmail.com>
2025-02-14 19:12:28 +0100

    join

--- knode.erl
+++ knode.erl
@@ -1,20 +1,33 @@
 -module(knode).
 -behaviour(gen_server).
--export([start_link/0, store/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-export([start_link/2, store/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 -export([ping/0, stop/0, get_id/0, read_store/0, initialize_kbuckets/1, find_node/1]).
 
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, #{}, []).
+start_link(Name, BootstrapNode) ->
+    gen_server:start_link({local, Name}, ?MODULE, #{bootstrap => BootstrapNode}, []).
 
 init(State) ->
     Id = generate_node_id(),
     io:format("Kademlia node starting, initial state: ~p, id: ~p~n", [State, Id]),
-    StoreTable = ets:new(kademlia_store, [set, named_table]),
-    KBuckets = ets:new(buckets, [set, named_table]),
+    StoreTable = ets:new(kademlia_store, [set]),
+    KBuckets = ets:new(buckets, [set]),
     % Inizializza la tabella KBuckets con 160 intervalli
     initialize_kbuckets(KBuckets),
-    % Inizializza lo stato con un contatore (che può servire per fare logging) e una tabella ETS
-    {ok, {Id, 0, State, StoreTable, KBuckets}}.
+    %recupero il nodo bootstrap
+    BootstrapNode = maps:get(bootstrap, State, undefined),
+
+    NewState = {Id, 0, State, StoreTable, KBuckets},
+    case BootstrapNode of
+        % Sono il primo nodo
+        undefined ->
+            io:format("Nodo ~p diventato bootstrap~n", [Id]),
+            {ok, NewState};
+        % Connetto al nodo bootstrap
+        _ ->
+            io:format("Nodo ~p si connette a bootstrap ~p~n", [Id, BootstrapNode]),
+            BootstrapNode ! {join_request, self()},
+            {ok, NewState}
+    end.
 
 handle_call({ping, Sender}, _From, {Id, Counter, State, StoreTable, KBuckets}) ->
     io:format("Node ~p (~p) received ping from ~p~n", [self(), Id, Sender]),
@@ -58,6 +71,17 @@ handle_cast(_Msg, State) ->
     io:format("Received cast message: ~p~n", [_Msg]),
     {noreply, State}.
 
+handle_info({join_request, NewNode}, {Id, Counter, State, StoreTable, KBuckets}) ->
+    io:format("Nodo ~p ricevuto join_request da ~p~n", [Id, NewNode]),
+    % aggiungo newnode ai kbuckets appropriati
+    % invia i miei k-buckets
+    NewNode ! {k_buckets, ets:tab2list(KBuckets)},
+    {noreply, {Id, Counter, State, StoreTable, KBuckets}};
+handle_info({k_buckets, Buckets}, {Id, Counter, State, StoreTable, KBuckets}) ->
+    io:format("Nodo ~p ricevuto k_buckets ~p~n", [Id, Buckets]),
+    % aggiungo i contatti a miei kbuckets
+    lists:foreach(fun(B) -> ets:insert(KBuckets, B) end, Buckets),
+    {noreply, {Id, Counter, State, StoreTable, KBuckets}};
 handle_info(_Info, State) ->
     io:format("Received info message: ~p~n", [_Info]),
     {noreply, State}.
@@ -82,7 +106,7 @@ read_store() ->
     gen_server:call(?MODULE, read_store).
 
 find_node(ToFindNodeId) ->
-    gen_server:call(?MODULE, {read_store, ToFindNodeId}).
+    gen_server:call(?MODULE, {find_node, ToFindNodeId}).
 
 generate_node_id() ->
     % 1. Genera un intero casuale grande (ad es. a 64 bit)
