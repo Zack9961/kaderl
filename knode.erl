2525811640e854cae690db36caf50232645d858d
Zack9961 <mrzack9099@gmail.com>
2025-02-21 00:15:27 +0100

    join funzionante

--- knode.erl
+++ knode.erl
@@ -1,6 +1,7 @@
 -module(knode).
 -behaviour(gen_server).
 -include_lib("stdlib/include/ms_transform.hrl").
+-define(K, 5).
 -export([start_link/2, store/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 -export([ping/0, stop/0, get_id/0, read_store/0, initialize_kbuckets/1, find_node/1, find_value/1]).
 
@@ -26,6 +27,7 @@ init(State) ->
         % Connetto al nodo bootstrap
         _ ->
             io:format("Nodo ~p si connette a bootstrap ~p~n", [Id, BootstrapNode]),
+            adding_bootstrap_node_in_kbuckets(BootstrapNode, KBuckets, Id),
             BootstrapNode ! {join_request, self(), Id},
             {ok, NewState}
     end.
@@ -87,10 +89,10 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     io:format("Received cast message: ~p~n", [_Msg]),
     {noreply, State}.
-
+%Guardare se deve essere call/cast
 handle_info({join_request, PIDNewNode, IdNewNode}, {Id, Counter, State, StoreTable, KBuckets}) ->
     io:format("Sono il Nodo ~p, ho ricevuto join_request da ~p~n", [Id, PIDNewNode]),
-    K = 5,
+
     % Calcolo la distanza tra il mio ID e l'ID del NewNode
     Distanza = calcola_distanza(Id, IdNewNode),
 
@@ -108,8 +110,11 @@ handle_info({join_request, PIDNewNode, IdNewNode}, {Id, Counter, State, StoreTab
     %Gestisco la dimensione massima del k-bucket
     NewNodes =
         if
-            length(UpdatedNodes) > K ->
+            length(UpdatedNodes) > ?K ->
                 % Rimuovi il nodo meno recentemente visto (alla testa della lista)
+                %Ricordarsi di aggiungere il ping, quindi di rimuoverlo se pingando non
+                %risponde, andando avanti per tutti i nodi seguenti, se tutti rispondono,
+                %non aggiungere il nodo
                 tl(UpdatedNodes);
             true ->
                 UpdatedNodes
@@ -119,12 +124,82 @@ handle_info({join_request, PIDNewNode, IdNewNode}, {Id, Counter, State, StoreTab
     ets:insert(KBuckets, {Key, NewNodes}),
 
     % invia i miei k-buckets
-    PIDNewNode ! {k_buckets, ets:tab2list(KBuckets)},
+    PIDNewNode ! {k_buckets, KBuckets},
     {noreply, {Id, Counter, State, StoreTable, KBuckets}};
-handle_info({k_buckets, Buckets}, {Id, Counter, State, StoreTable, KBuckets}) ->
-    io:format("Nodo ~p ricevuto k_buckets ~p~n", [Id, Buckets]),
-    % aggiungo i contatti a miei kbuckets
-    lists:foreach(fun(B) -> ets:insert(KBuckets, B) end, Buckets),
+handle_info({k_buckets, BucketsReceived}, {Id, Counter, State, StoreTable, KBuckets}) ->
+    io:format("Nodo ~p ricevuto k_buckets ~p~n", [Id, BucketsReceived]),
+    % 1. Converte la tabella ETS dei buckets in una lista
+    BucketsReceivedList = ets:tab2list(BucketsReceived),
+
+    % 2. Itera sulla lista di buckets ricevuti dal nodo bootstrap
+    lists:foreach(
+        fun({_, Nodes}) ->
+            % Itera sulla lista di nodi in ogni bucket
+            lists:foreach(
+                fun({NodePid, NodeId}) ->
+                    % Calcola la distanza tra il mio ID e l'ID del nodo corrente
+                    Distanza = calcola_distanza(Id, NodeId),
+
+                    case Distanza of
+                        0 ->
+                            % Non fare nulla, il nodo è se stesso
+                            ok;
+                        _ ->
+                            % Determina l'intervallo corretto per questa distanza
+                            RightKbucket = get_right_bucket_interval(Distanza, KBuckets),
+
+                            % Recupera i nodi attualmente presenti nel bucket corretto
+                            case RightKbucket of
+                                false ->
+                                    io:format(
+                                        "+++++ ERRORE: nessun bucket trovato per la distanza ~p (nodo ~p)~n",
+                                        [Distanza, NodeId]
+                                    );
+                                {_, _} ->
+                                    [{Key, CurrentNodes}] = ets:lookup(KBuckets, RightKbucket),
+
+                                    % Aggiungi il nuovo nodo alla lista (se non è già presente)
+                                    NewNodes =
+                                        case lists:keyfind(NodeId, 2, CurrentNodes) of
+                                            % Non trovato, lo aggiunge
+                                            false ->
+                                                io:format(
+                                                    "+++++ Aggiungo nodo ~p (id ~p) al bucket ~p~n",
+                                                    [
+                                                        NodePid, NodeId, RightKbucket
+                                                    ]
+                                                ),
+                                                CurrentNodes ++ [{NodePid, NodeId}];
+                                            % Già presente, non fare nulla
+                                            _ ->
+                                                io:format(
+                                                    "+++++ Nodo ~p (id ~p) già presente nel bucket ~p~n",
+                                                    [
+                                                        NodePid, NodeId, RightKbucket
+                                                    ]
+                                                ),
+                                                CurrentNodes
+                                        end,
+
+                                    % Gestisci la dimensione massima del k-bucket (se necessario)
+                                    NewNodesLimited =
+                                        if
+                                            length(NewNodes) > ?K ->
+                                                tl(NewNodes);
+                                            true ->
+                                                NewNodes
+                                        end,
+
+                                    % Aggiorna la tabella ETS con i nuovi nodi nel bucket
+                                    ets:insert(KBuckets, {Key, NewNodesLimited})
+                            end
+                    end
+                end,
+                Nodes
+            )
+        end,
+        BucketsReceivedList
+    ),
     {noreply, {Id, Counter, State, StoreTable, KBuckets}};
 handle_info(_Info, State) ->
     io:format("Received info message: ~p~n", [_Info]),
@@ -156,7 +231,7 @@ find_value(Key) ->
     gen_server:call(?MODULE, {find_value, Key}).
 
 generate_node_id() ->
-    % 1. Genera un intero casuale grande (ad es. a 64 bit)
+    % 1. Genera un intero casuale grande (64 bit)
     % Massimo valore per un intero a 64 bit
     RandomInteger = rand:uniform(18446744073709551615),
     % 2. Applica la funzione hash SHA-1
@@ -230,17 +305,28 @@ get_right_bucket_interval(Distanza, KBuckets) ->
             false
     end.
 
-% Funzione per aggiungere un nodo al bucket (gestendo l'eviction se necessario)
-add_node_to_bucket(NewNodeId, BucketNodes, K) ->
-    if
-        length(BucketNodes) < K ->
-            [NewNodeId | BucketNodes];
-        true ->
-            [_ | Rest] = BucketNodes,
-            [NewNodeId | Rest]
-    end.
-
 binary_to_integer_representation(Binary) ->
     io:format("Il binario grezzo è: ~p ~n", [Binary]),
     Bytes = binary_to_list(Binary),
     lists:foldl(fun(Byte, Acc) -> (Acc bsl 8) bor Byte end, 0, Bytes).
+
+adding_bootstrap_node_in_kbuckets(BootstrapPID, KBuckets, Id) ->
+    BootstrapID = gen_server:call(BootstrapPID, get_id),
+    io:format("Il bootstrap id è: ~p~n", [BootstrapID]),
+
+    % Calcolo la distanza tra il mio ID e l'ID del BootstrapNode
+    Distanza = calcola_distanza(Id, BootstrapID),
+
+    % Ottengo il giusto intervallo del k-bucket
+    RightKbucket = get_right_bucket_interval(Distanza, KBuckets),
+    io:format("Il kbucket giusto è: ~p~n", [RightKbucket]),
+
+    % Recupera il contenuto corrente del k-bucket
+    [{Key, CurrentNodes}] = ets:lookup(KBuckets, RightKbucket),
+    io:format("L'output di lookup è: ~p~n", [[{Key, CurrentNodes}]]),
+
+    %aggiungo in coda
+    UpdatedNodes = CurrentNodes ++ [{BootstrapPID, BootstrapID}],
+
+    %Reinserisco la tupla aggiornata nel k-bucket
+    ets:insert(KBuckets, {Key, UpdatedNodes}).
